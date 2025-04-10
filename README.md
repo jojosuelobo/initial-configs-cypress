b04055cb6810098c0219e230de1fa7f9306b3156
jojosuelobo <josuelobo2000@gmail.com>
2025-04-10 00:15:54 -0300

    Configurações detalhadas no cypress.config.js

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Instação 
+- npm init -y
+- npm install cypress 
+- npx cypress open
+- gitignore padrão
+
+2. Configurações detalhadas no cypress.config.js
+
--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,44 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    // Define a URL base para os testes
+    baseUrl: 'https://meusite.com', 
+
+    // Tempo padrão para comandos esperarem (ex: cy.get)
+    defaultCommandTimeout: 8000, 
+
+    // Variáveis de ambiente (acessível com Cypress.env)
+    env: {
+      ambiente: 'dev',
+      token: 'abc123'
+    }, 
+
+    // Intervalo entre digitação de cada caractere no cy.type()
+    keystrokeDelay: 20, 
+
+    // Tempo máximo que o Cypress espera para uma página carregar
+    pageLoadTimeout: 60000,
+
+    // Timeout para requisições cy.request()
+    requestTimeout: 10000, 
+
+    // Tempo para esperar resposta após envio de requisição
+    responseTimeout: 15000,
+
+    retries: {
+      runMode: 2, // Repetições no modo CI
+      openMode: 0 // Repetições no modo interativo
     },
+
+    // Tira screenshot automaticamente ao falhar um teste
+    screenshotOnRunFailure: true,
+
+    // Testes que levam mais tempo que isso serão marcados como "lentos"
+    slowTestThreshold: 10000, 
+
+    // Configuração da viewport
+    viewportHeight: 720,
+    viewportWidth: 1280,
   },
-});
+})
